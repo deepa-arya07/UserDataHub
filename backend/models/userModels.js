dd8247a6cc0d1248088eec7d5f2c087f04f8e377
deepa-arya07 <deepa.arya1540@gmail.com>
2025-01-25 21:44:01 +0530

    added backend api's

---
+++ backend/models/userModels.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  interest: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    requred: true,
+    unique: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+
+const router = express.Router();
+
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+module.exports = router;

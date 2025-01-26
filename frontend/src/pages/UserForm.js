23e0837718df65dc6ec9503d8dfb9a5efaa8052e
deepa-arya07 <deepa.arya1540@gmail.com>
2025-01-26 21:42:37 +0530

    updated ports and completed task

--- frontend/src/pages/UserForm.js
+++ frontend/src/pages/UserForm.js
@@ -26,7 +26,7 @@ const UserForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/users", {
+      const response = await fetch("http://localhost:8000/api/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
